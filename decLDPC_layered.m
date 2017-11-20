486393cacbd65a6282e6bdea47e35b693b182bdc
xiaoshaoning <xiaoshaoning@foxmail.com>
2017-11-20 21:22:14 +0800

    add LDPC decoding function

---
+++ decLDPC_layered.m
@@ -0,0 +1,108 @@
+% =========================================================================
+% Title       : LDPC decoder with layered detection schedule
+% File        : decLDPC_layered.m
+% -------------------------------------------------------------------------
+% Description :
+%   Layered LDPC detection according to E. Sharon, S. Litsyn, and J.
+%   Goldberger 2004. Supports SPA and MPA. Note that this LDPC decoder
+%   requires LLRs defined according to L=log(Pr[x=0]/Pr[x=1]).
+% ------------------------------------------------------------------------- 
+% Revisions   :
+%   Date       Version  Author  Description
+%   20-may-11  1.2      studer  cleanup for reproducible research
+%   04-jul-07  1.1      studer  some bugfixes
+%   02-jul-07  1.0      studer  file created
+% -------------------------------------------------------------------------
+%   (C) 2006-2011 Communication Theory Group                      
+%   ETH Zurich, 8092 Zurich, Switzerland                               
+%   Author: Dr. Christoph Studer (e-mail: studer@rice.edu)     
+% =========================================================================
+
+function [bit_output,LLR_D2,NumC,NumV] = decLDPC_layered(TxRx,LDPC,LLR_A2)
+
+  % -- initializations
+  numOfEntries = sum(sum(LDPC.H==1));
+  Rcv = spalloc(LDPC.par_bits,LDPC.tot_bits,numOfEntries); % msg matrix
+  LLR_D2 = LLR_A2; % initialize with input bitslis
+%   bit_output = zeros(1, LDPC.inf_bits);
+  NumC = 0; % number of computed check nodes
+  NumV = 0; % number of computed variable nodes   
+ 
+  % === TEST
+  vMask = ones(LDPC.par_bits,1); % indicates NCU needs work 
+  WORKLOAD = 0;
+  
+  % -- BEGIN loop over LDPC-internal iterations
+  for iter=1:TxRx.Decoder.LDPC.Iterations
+    
+    %%% % -- count the number of nonzero entries of H*x  
+    errors = 0;
+    
+    switch (TxRx.Decoder.LDPC.Type) 
+      case 'SPA', % -- sum-product algorithm                
+        % == for all parity check node
+        for j=1:LDPC.par_bits     
+          idx = find(LDPC.H(j,:)==1); % slow
+          S = LLR_D2(idx)-full(Rcv(j,idx));      
+          Spsi = sum(-log(1e-300+tanh(abs(S)*0.5)));
+          Ssgn = prod(sign(S));              
+          for v=1:length(idx)          
+            Qtemp = LLR_D2(idx(v)) - Rcv(j,idx(v));                
+            Qtemppsi = -log(1e-300+tanh(abs(Qtemp)*0.5));
+            Qtempsgn = Ssgn*sign(Qtemp);                
+            Rcv(j,idx(v)) = Qtempsgn*(-log(1e-300+tanh(abs(Spsi-Qtemppsi)*0.5)));        
+            LLR_D2(idx(v)) = Qtemp + Rcv(j,idx(v));        
+            % -- count number variable & check nodes computed
+            NumC = NumC + 1; 
+            NumV = NumV + 1;
+          end         
+          
+        end    
+      case 'MPA', % -- max-log approximation (max-product algorithm)
+        % == for all parity check node
+        for j=1:LDPC.par_bits               
+          idx = find(LDPC.H(j,:)==1); % slow      
+          S = LLR_D2(idx)-full(Rcv(j,idx));                  
+          for v=1:length(idx)             
+            Stmp = S;
+            Stmp(v) = []; % clear row                 
+            Rcv(j,idx(v)) = min(abs(Stmp))*prod(sign(Stmp));
+            LLR_D2(idx(v)) = S(v) + Rcv(j,idx(v));        
+            % --variable node & check node computed
+            NumC = NumC + 1; 
+            NumV = NumV + 1;
+          end
+        end       
+      case 'OMS', % -- offset min-sum [Chen05]
+        % == for all parity check node
+        for j=1:LDPC.par_bits               
+          idx = find(LDPC.H(j,:)==1); % slow      
+          S = LLR_D2(idx)-full(Rcv(j,idx));                  
+          for v=1:length(idx)                                        
+            Stmp = S;
+            Stmp(v) = []; % clear row            
+            % --> 0.3 seems to be good shift value (requires simulations)
+            Magnitude =  max(min(abs(Stmp))-0.3,0);
+            Signum = prod(sign(Stmp));            
+            Rcv(j,idx(v)) = Signum*Magnitude;
+            LLR_D2(idx(v)) = S(v) + Rcv(j,idx(v));        
+            % --variable node & check node computed
+            NumC = NumC + 1; 
+            NumV = NumV + 1;
+          end
+        end      
+      otherwise,
+        error('Unknown TxRx.Decoder.LDPC.Type.')
+    end         
+    
+  end
+ 
+  % -- compute binary-valued estimates
+  bit_output = 0.5*(1-mysign(LLR_D2(1:LDPC.inf_bits)));
+  
+  
+return
+
+function s = mysign(inp)
+  s = 2*double(inp>0)-1; 
+return
---
+++ test_ldpc.m
@@ -0,0 +1,55 @@
+% 5g ldpc encoding test
+% reference: 3GPP TS 38.212 section 5.3.2
+% author: Xiao, Shaoning 萧少宁
+% license: MIT
+% currently the only supported case is K = 8448
+
+function test_ldpc
+
+SNR_list = 15:15;
+
+trial_number = 1;
+
+BLER = zeros(1, length(SNR_list));
+
+TxRx.Decoder.LDPC.Iterations = 10;
+TxRx.Decoder.LDPC.Type = 'OMS';
+
+load H
+LDPC.H = H;
+[LDPC.par_bits,LDPC.tot_bits] = size(LDPC.H);
+LDPC.inf_bits = LDPC.tot_bits - LDPC.par_bits;
+
+for SNR_list_index = 1:length(SNR_list)
+    
+    sigma_square = 10^(-SNR_list(SNR_list_index)/10);
+    
+    for trial = 1:trial_number
+                
+        tx_bits = randi([0, 1], LDPC.inf_bits, 1);
+        
+        encoded_bits = ldpc_encode(tx_bits);
+        
+        symbols = 1 - 2 * encoded_bits;
+        
+        noise = randn(1, length(symbols));
+        
+        waveform = symbols + noise * sqrt(sigma_square);
+        
+        LLR_received = 2 * waveform / sigma_square;
+        
+        rx_bits = decLDPC_layered(TxRx, LDPC, LLR_received);
+        
+        rx_bits = rx_bits(:);
+        
+        if ~isequal(tx_bits, rx_bits)
+          BLER(SNR_list_index) = BLER(SNR_list_index) + 1;    
+        end
+        
+    end
+    
+end
+
+fprintf('BLER is %f\n', BLER/trial_number);
+
+end
