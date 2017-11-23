8f7083996b4fea4824d6d77d4a3e605a8afe7590
xiaoshaoning <xiaoshaoning@foxmail.com>
2017-11-23 23:19:28 +0800

    use more sparse matrices

--- ldpc_encode.m
+++ ldpc_encode.m
@@ -22,71 +22,71 @@ B_prime = BG_1(1:4, 23:26);
 C_prime = BG_1(5:46, 1:22);
 D_prime = BG_1(5:46, 23:26);
 
-A = zeros(4*z, 22*z);
+A = spalloc(4*z, 22*z, nnz(A_prime + ones(size(A_prime))));
 
 for row_index = 1:4
     for column_index = 1:22
         if A_prime(row_index, column_index) ~= -1
-            A((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = circshift(eye(z), -A_prime(row_index, column_index));
+            A((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = sparse(1:z, [A_prime(row_index, column_index)+1:z, 1:A_prime(row_index, column_index)], ones(1, z), z, z);
         else
-            A((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = zeros(z, z);
+            A((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = spalloc(z, z, 0);
         end
     end
 end
 
-B = zeros(4*z, 4*z);
+B = spalloc(4*z, 4*z, nnz(B_prime + ones(size(B_prime))));
 
 for row_index = 1:4
     for column_index = 1:4
         if B_prime(row_index, column_index) ~= -1
-            B((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = circshift(eye(z), -B_prime(row_index, column_index));
+            B((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = sparse(1:z, [B_prime(row_index, column_index)+1:z, 1:B_prime(row_index, column_index)], ones(1, z), z, z);
         else
-            B((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = zeros(z, z);
+            B((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = spalloc(z, z, 0);
         end
     end
 end
 
-C = zeros(42*z, 22*z);
+C = spalloc(42*z, 22*z, nnz(C_prime + ones(size(C_prime))));
 
 for row_index = 1:42
     for column_index = 1:22
         if C_prime(row_index, column_index) ~= -1
-            C((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = circshift(eye(z), -C_prime(row_index, column_index));
+            C((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = sparse(1:z, [C_prime(row_index, column_index)+1:z, 1:C_prime(row_index, column_index)], ones(1, z), z, z);
         else
-            C((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = zeros(z, z);
+            C((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = spalloc(z, z, 0);
         end
     end
 end
 
-D = zeros(42*z, 4*z);
+D = spalloc(42*z, 4*z, nnz(D_prime + ones(size(D_prime))));
 
 for row_index = 1:42
     for column_index = 1:4
         if D_prime(row_index, column_index) ~= -1
-            D((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = circshift(eye(z), -D_prime(row_index, column_index));
+            D((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = sparse(1:z, [D_prime(row_index, column_index)+1:z, 1:D_prime(row_index, column_index)], ones(1, z), z, z);
         else
-            D((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = zeros(z, z);
+            D((row_index-1)*z+1:row_index*z, (column_index-1)*z+1:column_index*z) = spalloc(z, z, 0);
         end
     end
 end
 
-B_inv = zeros(4*z, 4*z);
-B_inv(1:z, 1:z) = eye(z);
-B_inv(1:z, 1+z:2*z) = eye(z);
-B_inv(1:z, 1+2*z:3*z) = eye(z);
-B_inv(1:z, 1+3*z:4*z) = eye(z);
-B_inv(1+z:2*z, 1:z) = eye(z) + circshift(eye(z), -1);
-B_inv(1+z:2*z, 1+z:2*z) = circshift(eye(z), -1);
-B_inv(1+z:2*z, 1+2*z:3*z) = circshift(eye(z), -1);
-B_inv(1+z:2*z, 1+3*z:4*z) = circshift(eye(z), -1);
-B_inv(1+2*z:3*z, 1:z) = circshift(eye(z), -1);
-B_inv(1+2*z:3*z, 1+z:2*z) = circshift(eye(z), -1);
-B_inv(1+2*z:3*z, 1+2*z:3*z) = eye(z) + circshift(eye(z), -1);
-B_inv(1+2*z:3*z, 1+3*z:4*z) = eye(z) + circshift(eye(z), -1);
-B_inv(1+3*z:4*z, 1:z) = circshift(eye(z), -1);
-B_inv(1+3*z:4*z, 1+z:2*z) = circshift(eye(z), -1);
-B_inv(1+3*z:4*z, 1+2*z:3*z) = circshift(eye(z), -1);
-B_inv(1+3*z:4*z, 1+3*z:4*z) = eye(z) + circshift(eye(z), -1);
+B_inv = spalloc(4*z, 4*z, 20*z);
+B_inv(1:z, 1:z) = sparse(eye(z));
+B_inv(1:z, 1+z:2*z) = sparse(eye(z));
+B_inv(1:z, 1+2*z:3*z) = sparse(eye(z));
+B_inv(1:z, 1+3*z:4*z) = sparse(eye(z));
+B_inv(1+z:2*z, 1:z) = sparse(eye(z) + circshift(eye(z), -1));
+B_inv(1+z:2*z, 1+z:2*z) = sparse(circshift(eye(z), -1));
+B_inv(1+z:2*z, 1+2*z:3*z) = sparse(circshift(eye(z), -1));
+B_inv(1+z:2*z, 1+3*z:4*z) = sparse(circshift(eye(z), -1));
+B_inv(1+2*z:3*z, 1:z) = sparse(circshift(eye(z), -1));
+B_inv(1+2*z:3*z, 1+z:2*z) = sparse(circshift(eye(z), -1));
+B_inv(1+2*z:3*z, 1+2*z:3*z) = sparse(eye(z) + circshift(eye(z), -1));
+B_inv(1+2*z:3*z, 1+3*z:4*z) = sparse(eye(z) + circshift(eye(z), -1));
+B_inv(1+3*z:4*z, 1:z) = sparse(circshift(eye(z), -1));
+B_inv(1+3*z:4*z, 1+z:2*z) = sparse(circshift(eye(z), -1));
+B_inv(1+3*z:4*z, 1+2*z:3*z) = sparse(circshift(eye(z), -1));
+B_inv(1+3*z:4*z, 1+3*z:4*z) = sparse(eye(z) + circshift(eye(z), -1));
 
 s = s(:);
 
